0ec719f81cb80a4abfd531429f12c9c1271dfc93
kuqi@seatent.com <kuqi@seatent.com>
2019-10-15 10:07:07 +0800

    osiris配置

--- src/main/java/com/csy/mybatis/utils/DbUtils.java
+++ src/main/java/com/csy/mybatis/utils/DbUtils.java
@@ -214,8 +214,13 @@ public class DbUtils {
 
     private static String removePrefix(String tableName){
         String str = org.apache.commons.lang3.StringUtils.EMPTY;
-        if(tableName.startsWith("hd_pos")){
+        if(tableName.startsWith("hd_pos_")){
+            str = tableName.substring(7,tableName.length());
+            return str;
+        }
+        if(tableName.startsWith("hd_")){
             str = tableName.substring(3,tableName.length());
+            return str;
         }
         return str;
     }
